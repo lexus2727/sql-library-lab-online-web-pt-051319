4de88c8d788c69c58d44a5df1e38cbedd7361bc8
Candice Parker <lexus2424@gmail.com>
2019-07-26 17:03:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM series INNER JOIN authors ON series.author_id = authors.id INNER JOIN subgenres ON series.subgenre_id = subgenres.id;"
+  "SELECT authors.name, subgenres.name FROM series INNER JOIN authors ON series.author_id = authors.id INNER JOIN subgenres ON series.subgenre_id = subgenres.id GROUP BY series.title, species HAVING characters.species = 'human' ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_series_title_with_most_human_characters
